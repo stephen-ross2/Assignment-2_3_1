60d6c5478e141d908a47d8e11725cd0925ef1e6a
stephen-ross2 <stephen.ross2@outlook.com>
2024-11-14 14:07:55 -0600

    Added closing curly braces

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Assignment_2_3_1
     internal class Program
     {
         static void Main(string[] args)
-        { 
+        {
             string filePath = @"C:\Users\rosss\source\repos\Assignment 2_3_1\bin\Debug\net8.0\UserInfo.txt";
 
             List<string> lines = File.ReadAllLines(filePath).ToList();
@@ -20,41 +20,10 @@ namespace Assignment_2_3_1
 
             Console.ReadLine();
 
-
-           /* string filePath = "UserInfo.txt";
-
-            using (StreamWriter writer = new StreamWriter(filePath))
-
-            {
-                writer.WriteLine("Name: Steve Ross");
-                writer.WriteLine("Age: 34");
-                writer.WriteLine("Location: Shalimar, FL");
-
-            }
-
-            string currentDirectory = Directory.GetCurrentDirectory();
-            Console.WriteLine($"The file is saved in: {currentDirectory}\\{filePath}");
-
-            if (File.Exists(filePath))
-            {
-                Console.WriteLine("Reading details from the file:");
-                using (StreamReader reader = new StreamReader(filePath))
-
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-
-                    {
-                        Console.WriteLine(line);
-                    }
-                }
-            }
-
-            else
-            {
-                Console.WriteLine("File not found");
-                            } */
         }
     }
 }
+
+
+           
 
